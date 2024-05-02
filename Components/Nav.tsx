b9374884872dcc4569854ceae3df2a8cf9e4222d
Chris Anh Thien <chrisanhthien@Chriss-MacBook-Pro.local>
2024-05-02 03:09:42 -0400

    First code

---
+++ Components/Nav.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div className='w-[100%] top-0 h-[12vh] bg-[#14127] shadow-md'>
+        <div>
+            <h1>
+                WEB
+                <span>DEV</span>
+            </h1>
+            <div>Home</div>
+            <div>SERVICES</div>
+            <div>ABOUT</div>
+            <div>PROJECT</div>
+            <div>BLOG</div>
+            <div>CONTACT</div>
+        </div>
+
+    </div>
+  )
+}
+
+export default Nav
